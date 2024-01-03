6a08e195612a57befa1142bfe763406aee6596e6
yjb23tech <yjb23tech@gmail.com>
2024-01-03 15:36:04 +0000

    necessary pieces

---
+++ pages/SinglePageProfile.jsx
@@ -0,0 +1,21 @@
+import ProfileImage from "../public/images/personal_head_shot.jpg"
+
+const SinglePageProfile = () => {
+    return (
+        <div>
+            <img src="" alt="" />
+            <h1>Hi, my name is Joshua</h1>
+            <h2>Fun facts about me:</h2>
+            <ul>
+                <li>30 Years Old</li>
+                <li>Live in London</li>
+                <li>Trapper of the Year</li>
+            </ul>
+            <p>Please connect with me on Linkedin</p>
+            <input type="text">Enter email</input>
+            <button>Join newsletter</button>
+        </div>
+    )
+}
+
+export default SinglePageProfile; 
