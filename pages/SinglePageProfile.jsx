decbe5e4c3d8a4ed07b2ab3f01ced995650adeb5
yjb23tech <yjb23tech@gmail.com>
2024-01-03 16:44:18 +0000

    Crazy

--- pages/SinglePageProfile.jsx
+++ pages/SinglePageProfile.jsx
@@ -2,7 +2,7 @@ import ProfileImage from "../public/images/personal_head_shot.jpeg"
 
 const SinglePageProfile = () => {
     return (
-        <div>
+        <div className="personal-profile-main">
             <img src={ProfileImage} alt="Joshua Banjo's Headshot Photo" />
             <h1>Hi, my name is Joshua</h1>
             <h2>Fun facts about me:</h2>
@@ -12,8 +12,10 @@ const SinglePageProfile = () => {
                 <li>Trapper of the Year</li>
             </ul>
             <p>Please connect with me on Linkedin</p>
-            <input type="text" placeholder="Enter email..." />
-            <button>Join newsletter</button>
+            <div className="personal-profile-sub">
+                <input type="text" placeholder="Enter email..." />
+                <button>Join newsletter</button>
+            </div>
         </div>
     )
 }
