7aae3ff46ab92c88513cda8c3cc2d2a67d403f64
yjb23tech <yjb23tech@gmail.com>
2024-01-03 15:29:22 +0000

    Coming together slowly

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import { createRoot } from "react-dom/client"
+
+const App = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+const container = document.getElementById("root")
+const root = createRoot(container)
+root.render(<App />)
+
